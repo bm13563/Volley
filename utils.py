07b2f7a08b10df989505cf03c4306fb8f47f2d12
bm13563 <bm13563@my.bristol.ac.uk>
2021-03-26 18:32:48 +0000

    Changes to the utils.py file

--- utils.py
+++ utils.py
@@ -6,29 +6,30 @@ root_path = os.path.dirname(os.path.abspath(__file__))
 # set up argparser
 parser = argparse.ArgumentParser(description='Utilities file for running all scripts related to the Volley project')
 parser.add_argument("--test", action="store_true", help="run tests for the project")
-parser.add_argument("--run", nargs=1, help="run the flask project in a specific environment")
+parser.add_argument("--run", default=(False), help="run the flask project in a specific environment")
 
 
 # parse the input arguments
 args = parser.parse_args()
 argdict = vars(args)
+print(argdict)
 
 # ensure that only one utility is being used at a time
-used_args = [arg for arg in argdict.values() if arg is not None and arg]
+used_args = [arg for arg in argdict.values() if arg]
 if len(used_args) > 1:
     print("Please only use one utility argument at a time.")
     sys.exit()
 
 # run tests for the volley flask app
 # e.g "python utils.py --test"
-if "test" in argdict:
+if argdict["test"]:
     os.environ["FLASK_APP"] = "api"
     os.environ["FLASK_ENV"] = "development"
     os.system("python -m pytest")
 
 # run the volley flask app in the specified environment
 # e.g "python utils.py --run dev"
-if "run" in argdict:
+if argdict["run"]:
     if argdict["run"] == "dev":
         os.environ["FLASK_APP"] = "api"
         os.environ["FLASK_ENV"] = "development"
