ef7550e7c9533aa6b30cb7832b582c71645a0a39
bm13563 <bm13563@my.bristol.ac.uk>
2021-04-27 00:47:12 +0100

    Updated tests for logging out

--- tests/authentication/test_log_out.py
+++ tests/authentication/test_log_out.py
@@ -1,30 +1,13 @@
-# from tests.base import set_up, tear_down
-# from tests.authentication.test_registration import register
-# from tests.authentication.test_log_in import log_in
+from tests.base import set_up
+from tests.authentication.test_registration import register
+from tests.authentication.test_log_in import log_in
 
 
-# def log_out(client, args):
-#     return client.post("/auth/log_out", json=args, follow_redirects=True)
-
-
-# def test_successful_log_in():
-#     registration_args = {
-#         "name": "Big Benny M",
-#         "summary": "I like picking up litter",
-#         "interests": ["litter", "software"],
-#         "approximate_location": [-1.756465, 53.453474],
-#         "username": "bm13566@my.bristol.ac.uk",
-#         "password": "test_password1",
-#     }
-#     log_in_args = {
-#         "username": "bm13566@my.bristol.ac.uk",
-#         "password": "test_password1",
-#     }
-#     app, client = set_up()
-#     with client:
-#         register(client, registration_args)
-#         log_in(client, log_in_args)
-#         log_out_response = log_out(client, {})
-#         assert log_out_response.status == 200 or "200 OK"
-#         assert b"Successfully logged out!" in log_out_response.data
-#     tear_down()
+def test_successful_log_out():
+    app, client = set_up()
+    with client:
+        register(client)
+        log_in(client)
+        log_out_response = client.post("/auth/log_out", follow_redirects=True)
+        assert log_out_response.status == 200 or "200 OK"
+        assert b"Successfully logged out!" == log_out_response.data
