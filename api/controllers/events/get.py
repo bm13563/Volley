4ab15732d11f08cbee869fc06c9aabdcfe5b01d3
bm13563 <bm13563@my.bristol.ac.uk>
2021-04-27 23:03:31 +0100

    Added testing for getting an event that does not exist

--- api/controllers/events/get.py
+++ api/controllers/events/get.py
@@ -2,4 +2,8 @@ from ...models.events import Event
 
 
 def events_get(event_id):
-    return Event.objects.get(id=event_id).to_json()
+    try:
+        event = Event.objects.get(id=event_id).to_json()
+        return event
+    except Event.DoesNotExist:
+        return "This event does not exist"
--- tests/events/test_get.py
+++ tests/events/test_get.py
@@ -17,3 +17,17 @@ def test_successful_get():
         )
         assert get_response.status == 200 or "200 OK"
         assert get_data == json.loads(get_response.data)
+
+
+def test_failed_get():
+    app, client = set_up()
+    with client:
+        register(client)
+        log_in(client)
+        add_event(client)
+        get_response = client.post(
+            "/events/get/60872f44eecdc50c62b0de98",
+            follow_redirects=True,
+        )
+        assert get_response.status == 200 or "200 OK"
+        assert b"This event does not exist" in get_response.data
